0c2d6d09b42afd4dac5567af6226e59f258e9b5c
mridulgelal <mridul.gelal@gmail.com>
2025-03-05 22:20:03 +0545

    feat: import winston loki and call them in routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const client = require("prom-client");
 const responseTime = require("response-time");
 const { prettyHeavyRequest } = require("./utils");
+const { createLogger, transports } = require("winston");
+const LokiTransport = require("winston-loki");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,14 +11,31 @@ const PORT = process.env.PORT;
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics({ register: client.register });
 
+const options = {
+  transports: [
+    new LokiTransport({
+      host: "http://127.0.0.1:3100",
+    }),
+  ],
+};
+const logger = createLogger(options);
+
 const reqResTime = new client.Histogram({
   name: "http_express_req_res_time",
   help: "This tells you req res latancy ",
-  label: ["method", "route", "statuCode"],
+  labelNames: ["method", "route", "status_code"],
+  // data point, milliseconds
+  buckets: [1, 50, 100, 200, 400, 500, 800, 1000, 2000],
+});
+
+const totalReqCounter = new client.Counter({
+  name: "total_req",
+  help: "Tells total req",
 });
 
 app.use(
   responseTime((req, res, time) => {
+    totalReqCounter.inc();
     reqResTime
       .labels({
         method: req.method,
@@ -28,10 +47,13 @@ app.use(
 );
 
 app.get("/", (req, res) => {
+  logger.info("Req came on / router");
   return res.json({ message: "hello from express server" });
 });
 
 app.get("/slow", async (req, res) => {
+  logger.info("Req came on / slow");
+
   try {
     const timeTaken = await prettyHeavyRequest();
     return res.json({
@@ -39,6 +61,8 @@ app.get("/slow", async (req, res) => {
       message: `Heavy task completed in ${timeTaken}`,
     });
   } catch (error) {
+    logger.error(error.message);
+
     return res
       .status(500)
       .json({ status: "Error", error: "Internal server error" });
